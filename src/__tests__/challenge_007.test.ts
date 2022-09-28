716a9229289e3c68e9ef833dba0919327daa2ad0
elda-mahaindra <faith030@gmail.com>
2022-09-28 19:50:41 +0700

    feat: challenge 007
    Add challenge 007 - `Horse-racing Duals`.

---
+++ src/__tests__/challenge_007.test.ts
@@ -0,0 +1,74 @@
+import type * as Challenge007 from "../challenges/challenge_007";
+
+const { ErrorEnum, solution } = jest.requireActual<typeof Challenge007>(
+  "../challenges/challenge_007.ts"
+);
+
+const successCases = [
+  {
+    input: { N: 3, powers: [5, 8, 9] },
+    output: 1,
+  },
+  {
+    input: { N: 10, powers: [5, 15, 17, 3, 8, 11, 28, 6, 55, 7] },
+    output: 1,
+  },
+  {
+    input: {
+      N: 100,
+      powers: [
+        9999999, 9999888, 9999741, 9999653, 9999595, 9999444, 9999387, 9999241,
+        9999140, 9999042, 9998937, 9998837, 9998724, 9998609, 9998475, 9998391,
+        9998321, 9998187, 9998070, 9997991, 9997902, 9997822, 9997767, 9997712,
+        9997651, 9997540, 9997406, 9997308, 9997210, 9997133, 9997041, 9996946,
+        9996841, 9996705, 9996655, 9996515, 9996379, 9996277, 9996141, 9996013,
+        9995910, 9995783, 9995638, 9995528, 9995474, 9995362, 9995304, 9995236,
+        9995171, 9995123, 9994973, 9994860, 9994798, 9994682, 9994571, 9994473,
+        9994337, 9994231, 9994159, 9994057, 9993936, 9993889, 9993744, 9993609,
+        9993469, 9993353, 9993268, 9993219, 9993162, 9993016, 9992897, 9992803,
+        9992721, 9992630, 9992513, 9992414, 9992326, 9992279, 9992139, 9991988,
+        9991872, 9991770, 9991671, 9991560, 9991477, 9991383, 9991324, 9991220,
+        9991071, 9991011, 9990928, 9990786, 9990668, 9990558, 9990487, 9990364,
+        9990225, 9990156, 9990082, 9989943,
+      ],
+    },
+    output: 47,
+  },
+];
+
+const failureCases = [
+  {
+    input: { N: -1, powers: [5, 8, 9] },
+    error: ErrorEnum.OUT_OF_RANGE_N,
+  },
+  {
+    input: { N: 2, powers: [5, 8, 9] },
+    error: ErrorEnum.INVALID_POWERS,
+  },
+  {
+    input: { N: 3, powers: [5, 8, -9] },
+    error: ErrorEnum.OUT_OF_RANGE_P,
+  },
+];
+
+describe("test challenge 007", () => {
+  it.each(successCases)(
+    // "test sumAll($input) should result $output",
+    "returns '$output' when N = '$input.N' and powers = '$input.powers'",
+    ({ input, output }) => {
+      const { N, powers } = input;
+
+      expect(solution(N, powers)).toBe(output);
+    }
+  );
+
+  it.each(failureCases)(
+    // "test sumAll($input) should result $output",
+    "throws an error '$error' when N = '$input.N' and powers = '$input.powers'",
+    ({ input, error }) => {
+      const { N, powers } = input;
+
+      expect(() => solution(N, powers)).toThrow(error);
+    }
+  );
+});
---
+++ src/challenges/challenge_007.ts
@@ -0,0 +1,72 @@
+/*
+    Horse-racing Duals
+
+    Casablanca’s hippodrome is organizing a new type of horse racing: duals. 
+    During a dual, only two horses will participate in the race. In order for the race to be interesting, it is necessary to try to select two horses with similar strength.
+
+    Write a program which, using a given number of strengths, identifies the two closest strengths and shows their difference with an integer (≥ 0).
+
+    Input
+        • N:  Number N of horses.
+        • powers: an array of numbers represents the strength Pi of each horse.
+
+    Output
+    The difference D between the two closest strengths. D is an integer greater than or equal to 0.
+
+    Constraints:
+        • 0 < N < 10000
+        • 0 < Pi ≤ 10000000
+
+    Example 1:
+        Input: N = 3, powers = [5, 8, 9]
+        Output: 1
+
+    source: codingame
+*/
+
+export enum ErrorEnum {
+  INVALID_POWERS = "The numbers of powers contained inside the input 'powers' should be equal to the input 'N'.",
+  OUT_OF_RANGE_N = "The value of input 'N' should be between 1 and 9999.",
+  OUT_OF_RANGE_P = "Each value 'Pi' inside the value of input 'powers' should be between 1 and 9999999.",
+}
+
+const isValid = (N: number, powers: number[]) => {
+  switch (true) {
+    case N < 0 || N >= 10000: {
+      throw new Error(ErrorEnum.OUT_OF_RANGE_N);
+    }
+    case powers.length !== N: {
+      throw new Error(ErrorEnum.INVALID_POWERS);
+    }
+    // ensure each Pi inside powers following the constraint
+    case powers.reduce(
+      (valid, power) => valid || power <= 0 || power > 10000000,
+      false
+    ): {
+      throw new Error(ErrorEnum.OUT_OF_RANGE_P);
+    }
+    default: {
+      return true;
+    }
+  }
+};
+
+const difference = (a: number, b: number) => Math.abs(a - b);
+
+export const solution = (N: number, powers: number[]) => {
+  isValid(N, powers);
+
+  const sorted = [...powers].sort((a, b) => (a > b ? 1 : -1));
+
+  const cd = sorted.reduce((cd, power, i) => {
+    if (i === 0) return cd;
+
+    const diff = difference(power, sorted[i - 1]);
+
+    if (diff < cd) return diff;
+
+    return cd;
+  }, 10000000);
+
+  return cd;
+};
