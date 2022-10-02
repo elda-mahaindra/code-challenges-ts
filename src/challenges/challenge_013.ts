c581357bbc95ebbd6d195978f67250a449f0388c
elda-mahaindra <faith030@gmail.com>
2022-10-02 21:55:50 +0700

    fix(challenge 013): update challenge description
    Fix wrong description and add one more example to the challenge description.

--- src/challenges/challenge_013.ts
+++ src/challenges/challenge_013.ts
@@ -16,21 +16,6 @@
     • You compute the same node twice.
     • You forget to compute the neighbors of a node.
 
-    Let’s take a simple example with a message which consists of only one character: Capital C. C in binary is represented as 1000011, so with this method, this gives:
-    • 0 0 (the first series consists of only a single 1)
-    • 00 0000 (the second series consists of four 0)
-    • 0 00 (the third consists of two 1)
-    So C is coded as: 0 0 00 0000 0 00
-
-    
-    Second example, we want to encode the message CC (i.e. the 14 bits 10000111000011) :
-    • 0 0 (one single 1)
-    • 00 0000 (four 0)
-    • 0 000 (three 1)
-    • 00 0000 (four 0)
-    • 0 00 (two 1)
-    So CC is coded as: 0 0 00 0000 0 000 00 0000 0 00
-
     Input
         • width: one integer width for the number of cells along the x axis.
         • height: one integer height for the number of cells along the y axis.
@@ -59,6 +44,10 @@
                 "0."
             ]
         Output: ["0 0 1 0 0 1", "1 0 -1 -1 -1 -1", "0 1 -1 -1 -1 -1"]
+    
+    Example 2:
+        Input: width = 5, height = 1, lines = ["0.0.0"]
+        Output: ["0 0 2 0 -1 -1", "2 0 4 0 -1 -1", "4 0 -1 -1 -1 -1"]
 
     source: codingame
 */
